05280ada7bad90d370ec76f663eeb7a5e1501239
td <15023474083@163.com>
2020-07-31 16:16:03 +0800

    新增侧滑控件

--- app/src/main/java/com/tan/tools/MainActivity.kt
+++ app/src/main/java/com/tan/tools/MainActivity.kt
@@ -2,10 +2,14 @@ package com.tan.tools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnDelete.setOnClickListener{
+            swipe_menu_layout.quickClose()
+        }
     }
 }
